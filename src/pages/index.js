667cba1d16ad6a62a5fbc851563637b83410a1d9
Jamie Liu <jamie.liu386@gmail.com>
2020-05-16 16:36:53 -0400

    fix ESLintError

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,5 @@
 import React from "react"
-import { Link } from "gatsby"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
 import HothDescription from "../components/HomePage/HothDescription"
 
 const IndexPage = () => (
