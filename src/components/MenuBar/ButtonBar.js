aaa3698de40ba356c4d2020d9b7bd5674f7edb33
Miles Wu <mileswu13@gmail.com>
2020-05-30 13:49:40 -0700

    Nav button style changes
    Styles added to "Apply" button as per the figma.

--- src/components/MenuBar/ButtonBar.js
+++ src/components/MenuBar/ButtonBar.js
@@ -2,15 +2,31 @@ import React from 'react';
 import Button from '@material-ui/core/Button';
 import PropTypes from 'prop-types';
 
+import classNames from 'classnames';
 import { makeStyles } from '@material-ui/core/styles';
 
 import LinkNoStyle from '../LinkNoStyle/LinkNoStyle';
 
 const useStyles = makeStyles(theme => {
+	const menuBarAdaptiveThreshold = theme.breakpoints.values.sm * 1.3;
 	return {
 		btn: {
 			fontFamily: theme.typography.fontFamily,
 			fontWeight: 500
+		},
+		borderBtn: {
+			margin: 10,
+			transition: theme.transitions.create(['color', 'background'], {
+				duration: theme.transitions.duration.complex
+			}),
+			'&:hover': {
+				color: '#FFFFFF',
+				background: '#DB99FD'
+			},
+			[theme.breakpoints.down(menuBarAdaptiveThreshold)]: {
+				// mobile
+				margin: 0
+			}
 		}
 	};
 });
@@ -25,7 +41,7 @@ function ButtonBar({ isMobile }) {
 
 	const BorderLink = ({ to, ...props }) =>
 		<LinkNoStyle>
-			<Button variant='outlined' fullWidth={isMobile} className={classes.btn} {...props} />
+			<Button variant='outlined' fullWidth={isMobile} className={classNames(classes.btn, classes.borderBtn)} {...props} />
 		</LinkNoStyle>;
 	/* eslint-enable */
 
