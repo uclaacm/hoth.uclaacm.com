1367eb04d6cb11ec5afcc288636dc8dea00ed9da
Timothy Rediehs <trediehs@g.ucla.edu>
2020-06-23 00:26:00 -0700

    Adjusted to new homepage

--- src/components/MenuBar/ButtonBar.js
+++ src/components/MenuBar/ButtonBar.js
@@ -51,7 +51,7 @@ function ButtonBar({ isMobile }) {
 		},
 		{
 			name: 'Schedule',
-			to: '/'
+			to: '/schedule'
 		},
 		{
 			name: 'Prizes',
--- src/components/SchedulePage/Schedule.js
+++ src/components/SchedulePage/Schedule.js
@@ -9,28 +9,57 @@ const testEvents = [
 		name: 'ACM-W X ACM Hack | Hackathon 101 + Team Formation',
 		location: 'Sycamore Room',
 		// eslint-disable-next-line no-multi-str
-		description: 'First hackathon? We\'ll show you all the tips and tricks you\'ll\
-		 need to know to be successful and most importantly, have fun!',
+		description: 'First hackathon? We\'ll show you all the tips and tricks you\'ll ' +
+		'need to know to be successful and most importantly, have fun!',
 		startTime: '9:30 AM'
 	},
 	{
 		name: 'Git and Github',
-		location: 'Bolter Hall',
+		location: 'De Neve Auditorium',
 		// eslint-disable-next-line no-multi-str
-		description: 'With multiple developers each working on \
-		a different feature of a project, bringing things together \
-		can get messy pretty easily. Come learn how to use Git to \
-		manage code across multiple collaborators and versions!',
+		description: 'With multiple developers each working on ' +
+		'a different feature of a project, bringing things together ' +
+		'can get messy pretty easily. Come learn how to use Git to ' +
+		'manage code across multiple collaborators and versions!',
 		startTime: '10:00 AM'
 	},
 	{
 		name: 'INTRO TO IOS',
 		location: 'Sycamore Room',
 		// eslint-disable-next-line no-multi-str
-		description: 'Learn all about the fundamentals of iOS development\
-		 by making a simple app in Swift and XCode! \
-		 You must have a Mac to be able to code along in this workshop.',
+		description: 'Learn all about the fundamentals of iOS development ' +
+		'by making a simple app in Swift and XCode! ' +
+		'You must have a Mac to be able to code along in this workshop.',
 		startTime: '10:00 AM'
+	},
+	{
+		name: 'FACEBOOK | ZERO TO HERO WITH REACT NATIVE',
+		location: 'De Never Auditorium ',
+		// eslint-disable-next-line no-multi-str
+		description: 'In this workshop we will learn how to quickly create a mobile app' +
+		'using React Native. We will be covering the React fundamentals (such as props' +
+		'and state) and apply them to quickly build a simple cross-platform app using' +
+		'React Native. This will be a hands-on workshop, so come in with the intent to hack!' +
+		'If possible, come in with Node.js, Git, and a text editor of your choice installed already!',
+		startTime: '11:00 AM'
+	},
+	{
+		name: 'INTRO TO WEB DEV: HTML AND CSS',
+		location: 'Sycamore Room',
+		// eslint-disable-next-line no-multi-str
+		description: 'Ever wanted to create and design your own website?' +
+		'Come learn about HTML and CSS, the basic components behind every' +
+		'website, and how you can use them to build unique and impressive web pages!',
+		startTime: '11:00 AM'
+	},
+	{
+		name: 'INTRO TO ANDROID',
+		location: 'De Neve Learning Center',
+		// eslint-disable-next-line no-multi-str
+		description: 'Learn all about the fundamentals of iOS development ' +
+		'by making a simple app in Swift and XCode! ' +
+		'You must have a Mac to be able to code along in this workshop.',
+		startTime: '11:00 AM'
 	}
 ];
 
@@ -38,6 +67,7 @@ const useStyles = makeStyles(theme => ({
 	title: {
 		fontFamily: theme.typography.fontFamily,
 		fontWeight: 'bold',
+		paddingTop: 24,
 		paddingBottom: 16
 	}
 }));
