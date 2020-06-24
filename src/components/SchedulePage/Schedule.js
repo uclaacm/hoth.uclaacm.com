31cf042194e0d147b6d1ce417fd96486245dd17a
Timothy Rediehs <trediehs@g.ucla.edu>
2020-06-23 17:12:17 -0700

    Cleanup

--- src/components/SchedulePage/Schedule.js
+++ src/components/SchedulePage/Schedule.js
@@ -12,7 +12,6 @@ const events = [
 			{
 				name: 'ACM-W X ACM Hack | Hackathon 101 + Team Formation',
 				location: 'Sycamore Room',
-				// eslint-disable-next-line no-multi-str
 				description: <>First hackathon? We’ll show you all the tips and tricks you’ll
 				need to know to be successful and most importantly, have fun!</>
 			}
@@ -24,7 +23,6 @@ const events = [
 			{
 				name: 'Git and Github',
 				location: 'De Neve Auditorium',
-				// eslint-disable-next-line no-multi-str
 				description: <>With multiple developers each working on
 				a different feature of a project, bringing things together
 				can get messy pretty easily. Come learn how to use Git to
@@ -33,7 +31,6 @@ const events = [
 			{
 				name: 'INTRO TO IOS',
 				location: 'Sycamore Room',
-				// eslint-disable-next-line no-multi-str
 				description: <>Learn all about the fundamentals of iOS development
 				by making a simple app in Swift and XCode!
 				You must have a Mac to be able to code along in this workshop.</>
@@ -46,7 +43,6 @@ const events = [
 			{
 				name: 'FACEBOOK | ZERO TO HERO WITH REACT NATIVE',
 				location: 'De Never Auditorium ',
-				// eslint-disable-next-line no-multi-str
 				description: <>In this workshop we will learn how to quickly create a mobile app
 				using React Native. We will be covering the React fundamentals (such as props
 				and state) and apply them to quickly build a simple cross-platform app using
@@ -56,7 +52,6 @@ const events = [
 			{
 				name: 'INTRO TO WEB DEV: HTML AND CSS',
 				location: 'Sycamore Room',
-				// eslint-disable-next-line no-multi-str
 				description: <>Ever wanted to create and design your own website?
 				Come learn about HTML and CSS, the basic components behind every
 				website, and how you can use them to build unique and impressive web pages!</>
@@ -64,7 +59,6 @@ const events = [
 			{
 				name: 'INTRO TO ANDROID',
 				location: 'De Neve Learning Center',
-				// eslint-disable-next-line no-multi-str
 				description: <>Learn all about the fundamentals of iOS development
 				by making a simple app in Swift and XCode!
 				You must have a Mac to be able to code along in this workshop.</>
@@ -81,10 +75,7 @@ const useStyles = makeStyles(theme => ({
 		paddingBottom: 16
 	}
 }));
-/*
-Organize the events into bins by time and display each bin as a time slot
-NOT DONE. VERY BAD. DO NOT COMMIT.
-*/
+
 const SchedulePage = () => {
 	const classes = useStyles();
 	return <>
