9f6aac89f159c9eb7f2f0d4d6e1f33ba8d48e064
Miles Wu <mileswu13@gmail.com>
2020-07-12 09:20:45 -0700

    Change favicon to hack logo (#61)
    Hoth website favicon updated from default gatsby icon to use our hack logo in
    accordance with #39.
    
    Fixes: #39
    
    Default favicon image removed from directory. No longer necessary since favicon
    now uses UCLA ACM Hack logo.

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,7 +29,7 @@ module.exports = {
 				background_color: '#fff',
 				theme_color: '#c960ff',
 				display: 'minimal-ui',
-				icon: 'src/images/gatsby-icon.png' // This path is relative to the root of the site.
+				icon: 'src/images/hack-logo.svg' // This path is relative to the root of the site.
 			}
 			/* eslint-enable camelcase */
 		},
