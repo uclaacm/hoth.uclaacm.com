c476d051df978d5a3a398c36117aa1f789c69fd7
milesswu <mileswu13@gmail.com>
2020-08-16 12:04:10 -0700

    Redesign of Upcoming Workshops section
    Upcoming workshops section now contains more whitespace so as not to prevent the
    feeling of information being cramped. Events are also more distinctly separated
    using a thick divider between each one.

--- src/components/HomePage/UpcomingEvents.js
+++ src/components/HomePage/UpcomingEvents.js
@@ -2,25 +2,35 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
-import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
+import Divider from '@material-ui/core/Divider';
 import { Link } from 'gatsby';
 import workshopSchedule from '../../data/WorkshopSchedule';
-import TimeSlot from './TimeSlot';
+import Event from './Event';
+import { currentTimeZoneShort } from '../../utils/timezone_names.js';
+
+const timeFormatter = new Intl.DateTimeFormat('en-US', { hour: 'numeric', minute: '2-digit' });
+
 
 const useStyles = makeStyles(theme => ({
 	title: {
 		fontFamily: theme.typography.fontFamily,
 		fontWeight: 'bold',
-		paddingTop: '24px',
-		paddingBottom: '16px'
+		padding: '1.2em 0'
 	},
 	eventsContainer: {
-		paddingTop: '16px',
-		paddingBottom: '24px'
+		display: 'flex',
+		flexDirection: 'column',
+		justifyContent: 'center',
+		minHeight: '100vh'
 	},
 	moreWorkshopsButton: {
-		marginTop: '8px'
+		alignSelf: 'center',
+		marginTop: '3.5em'
+	},
+	divider: {
+		margin: '3.5em 0 3.5em 3em',
+		height: '2px'
 	}
 }));
 
@@ -29,28 +39,42 @@ function UpcomingEvents() {
 	const currentTime = new Date();
 	let eventsDisplayed = 0;
 	const numEventsToDisplay = 2;
-	const timeSlots = [];
+
+	const events = [];
 	// Assumes workshopSchedule is ordered by time
 	for (let i = 0; i < workshopSchedule.length && eventsDisplayed < numEventsToDisplay; i++) {
-		const workshop = workshopSchedule[i];
-		if (currentTime < workshop.startTime) {
-			timeSlots.push(workshop);
-			eventsDisplayed += workshop.events.length;
+		const timeSlot = workshopSchedule[i];
+		if (currentTime < timeSlot.startTime) {
+			timeSlot.events.forEach(event => {
+				const eventFormat = {
+					startTime: `${timeFormatter.format(timeSlot.startTime)} ${currentTimeZoneShort}`,
+					...event
+				};
+				events.push(eventFormat);
+			});
+			eventsDisplayed += timeSlot.events.length;
 		}
 	}
 	if (eventsDisplayed === 0) {
 		return null; // Abort entirely
 	}
+	const renderEvents = eventsDisplayed === 0 ?
+		null :
+		events.map((event, index) => {
+			return (
+				<>
+					<Event key={index} {...event} />
+					{index === events.length - 1 ? null : <Divider variant='inset' className={classes.divider} />}
+				</>
+			);
+		});
+
 	return <Container maxWidth='md' className={classes.eventsContainer}>
-		<Grid container justify='center' spacing={1}>
-			<Typography align='center' component='h1' variant='h4'
-				className={classes.title}>Upcoming Workshops</Typography>
-			{timeSlots.map(timeslot => {
-				return <TimeSlot key={timeslot.startTime} time={timeslot.startTime} events={timeslot.events} />;
-			})}
-			<Button component={Link} role='link' className={classes.moreWorkshopsButton}
-				variant='outlined' to='/schedule'>More Workshops</Button>
-		</Grid>
+		<Typography align='left' component='h1' variant='h4'
+			className={classes.title}>Upcoming Workshops</Typography>
+		{renderEvents}
+		<Button component={Link} role='link' className={classes.moreWorkshopsButton}
+			variant='outlined' to='/schedule'>More Workshops</Button>
 	</Container>;
 }
 
