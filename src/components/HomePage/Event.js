c64edc6447e4eefb3434bd7d2d0a17986082e470
milesswu <mileswu13@gmail.com>
2020-08-16 12:01:42 -0700

    New design for homepage event component
    A new component created for the homepage's upcoming workshops section. This
    component follows the style of our redesign, which shows each event as
    standalone with its own time (rather than showing events as part of an overall
    timeslot).

---
+++ src/components/HomePage/Event.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles(theme => ({
+	name: {
+		color: theme.palette.secondary.main,
+		fontWeight: 'bold',
+		textTransform: 'uppercase'
+	},
+	subtitle: {
+		fontWeight: theme.typography.fontWeightMedium,
+		color: '#858585',
+		display: 'flex',
+		alignItems: 'center',
+		marginTop: '1em'
+	},
+	firstSubtitleItem: {
+		marginRight: '1.5em',
+		fontSize: '1.1em'
+	},
+	description: {
+		fontSize: '1em',
+		[theme.breakpoints.up('md')]: {
+			paddingLeft: '2.75em'
+		}
+	}
+}));
+
+function Event({ startTime, name, location, description }) {
+	const classes = useStyles();
+	return (
+		<>
+			<Grid container spacing={1}>
+				<Grid item sm={12} md={4}>
+					<Typography component='h3' variant='subtitle1' className={classes.name}>
+						{name}
+					</Typography>
+					<Box className={classes.subtitle}>
+						<Typography variant='body2' className={classes.firstSubtitleItem}>
+							{startTime}
+						</Typography>
+						<Typography variant='body2' >
+							{location}
+						</Typography>
+					</Box>
+				</Grid>
+				<Grid item sm={12} md={8}>
+					<Typography variant='body1' className={classes.description}>
+						{description}
+					</Typography>
+				</Grid>
+			</Grid>
+		</>
+	);
+}
+
+Event.propTypes = {
+	startTime: PropTypes.string.isRequired,
+	name: PropTypes.string.isRequired,
+	location: PropTypes.string.isRequired,
+	description: PropTypes.string.isRequired
+};
+
+export default Event;
