bed864b45e8e71d24cd3b30b1169d0a7330f48d5
Jamie Liu <jamie.liu386@gmail.com>
2020-08-16 13:55:08 -0400

    Miscellaneous fixes

--- src/components/HomePage/HothDescription.js
+++ src/components/HomePage/HothDescription.js
@@ -1,67 +1,85 @@
 import React from 'react';
 import { Container, Typography, Grid, Box } from '@material-ui/core';
 import { useTheme } from '@material-ui/core/styles';
-// import useMediaQuery from '@material-ui/core/useMediaQuery';
+import makeStyles from '@material-ui/core/styles/makeStyles';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 
 import hothImage1 from '../../images/homepage-carousel/IMG_1914.jpg';
 import hothImage2 from '../../images/homepage-carousel/IMG_2030.jpg';
 import fbLogo from '../../images/fb_logo.png';
 
-const eventURL = 'https://www.facebook.com';
+const eventURL = '';
+
+const useStyles = makeStyles(theme => ({
+	subtitle: {
+		fontSize: '3em',
+		fontWeight: theme.typography.fontWeightBold,
+		color: '#bbb',
+		[theme.breakpoints.down('sm')]: {
+			fontSize: '2.5em'
+		},
+		[theme.breakpoints.down('xs')]: {
+			fontSize: '2em'
+		}
+	},
+	title: {
+		textTransform: 'uppercase',
+		textAlign: 'center',
+		fontSize: '4em',
+		fontWeight: theme.typography.fontWeightBold,
+		color: theme.palette.primary.main,
+		[theme.breakpoints.down('sm')]: {
+			fontSize: '3.5em'
+		},
+		[theme.breakpoints.down('xs')]: {
+			fontSize: '2.5em'
+		}
+	}
+}));
 
 export default function HothDescription() {
 	const theme = useTheme();
-	// const isSmall = useMediaQuery(theme.breakpoints.down('sm'));
+	const classes = useStyles();
+	const isSmall = useMediaQuery(theme.breakpoints.down('sm'));
 
 	return (
 		<Container maxWidth='md' style={{ paddingTop: '40px' }}>
-			<Typography variant='h5' style={{
-				fontSize: '3em',
-				fontWeight: 700,
-				color: '#bbbbbb'
-			}}>What’s</Typography>
-			<Typography variant='h4' style={{
-				textTransform: 'uppercase',
-				textAlign: 'center',
-				fontSize: '4em',
-				fontWeight: 700,
-				color: theme.palette.primary.main,
-				paddingBottom: 80
-			}}>Hack on the Hill?</Typography>
-
-			<Grid container spacing={8}>
-				<Grid item md={6}>
+			<hgroup>
+				<Typography variant='h2' className={classes.subtitle}>What’s</Typography>
+				<Typography variant='h2' className={classes.title} style={{
+					paddingBottom: isSmall ? theme.spacing(2) : theme.spacing(8)
+				}}>Hack on the Hill?</Typography>
+			</hgroup>
+			<Grid container spacing={isSmall ? 3 : 8} alignItems='center' justify='center'>
+				<Grid item sm={8} md={6}>
 					<img src={hothImage1} width='100%' style={{
 						borderRadius: '14px'
 					}}/>
 				</Grid>
-				<Grid item md={6}>
-					<Typography variant="body1" style={{ paddingTop: 20 }}>
-						Hack on the Hill (HOTH) 7 is a 12-hour,
-						<Box fontWeight='bold' component="span">
-							{` beginner-friendly `}
-						</Box>
-						hackathon designed to give beginners a glimpse into what a real
+				<Grid item sm={10} md={6}>
+					<Typography variant='body1' style={{ paddingBottom: isSmall ? theme.spacing(2) : 0 }}>
+						Hack on the Hill (HOTH) 7 is a 12-hour,{' '}
+						<strong>beginner-friendly</strong>
+						{' '}hackathon designed to give beginners a glimpse into what a real
 						hackathon would be and feel like.
 					</Typography>
 				</Grid>
 			</Grid>
-			<Grid container spacing={8}>
-				<Grid item md={6}>
-					<Typography variant='body1' component='p' style={{ paddingTop: 20 }}>
+			<Grid container spacing={ isSmall ? 3 : 8 } wrap='wrap-reverse' justify='center' alignItems='center'>
+				<Grid item sm={10} md={6}>
+					<Typography
+						variant='body1'
+						component='p'
+						style={{ paddingBottom: isSmall ? theme.spacing(2) : 0 }}>
 						During HOTH, there will be
-						<Box fontWeight='bold' component="span">{` workshops`}</Box>,
-						<Box fontWeight='bold' component="span">{` mentors`}</Box>, and
-						<Box fontWeight='bold' component="span">{` amazing prizes `}</Box>
+						{` `}<strong>workshops</strong>,
+						{` `}<strong>mentors</strong>, and
+						{` `}<strong>amazing prizes</strong>{' '}
 						for the best hacks. We will also be providing a selection of
 						hardware for hackers to check out and hack with.
 					</Typography>
-					<Typography variant='body1' component='p' style={{ paddingTop: '2ex' }}>
-						Learn more about our planned workshops, prize tracks and hardware
-						below!
-					</Typography>
 				</Grid>
-				<Grid item md={6}>
+				<Grid item sm={8} md={6}>
 					<img src={hothImage2} width='100%' style={{
 						borderRadius: '14px'
 					}}/>
@@ -70,12 +88,14 @@ export default function HothDescription() {
 
 			{ eventURL === '' ?
 				null :
-				<Box display='flex' flexDirection='column' alignItems='center'>
+				<Box
+					display='flex'
+					flexDirection='column'
+					alignItems='center'
+					width={ isSmall ? '80%' : '40%' }
+					margin='0 auto'>
 					<Typography variant='body1' style={{
-						textAlign: 'center',
-						width: '40%',
-						paddingTop: 80,
-						margin: '0 auto'
+						textAlign: 'center'
 					}}>
 						Check out our Facebook event to stay tuned for additional updates!
 					</Typography>
