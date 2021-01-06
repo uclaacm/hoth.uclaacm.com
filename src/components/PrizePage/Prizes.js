4745886042beab9b390b93b84719a3a841eaa8e0
Timothy Rediehs <trediehs@g.ucla.edu>
2021-01-06 00:19:03 -0800

    add gatsby image

--- src/components/PrizePage/Prizes.js
+++ src/components/PrizePage/Prizes.js
@@ -1,81 +1,63 @@
 import React from 'react';
 
+import { basename } from 'path';
+import { graphql, useStaticQuery } from 'gatsby';
 import Box from '@material-ui/core/Box';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import MuiTableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-
-import useMediaQuery from '@material-ui/core/useMediaQuery';
-import useTheme from '@material-ui/core/styles/useTheme';
+import grey from '@material-ui/core/colors/grey';
 import makeStyles from '@material-ui/core/styles/makeStyles';
-import withStyles from '@material-ui/core/styles/withStyles';
-
-import DesignHack from './images/design-hack.png';
-import GameHack from './images/game-hack.png';
-import MLHack from './images/ml-hack.png';
-import MobileHack from './images/mobile-hack.png';
-import OverallHack from './images/overall-hack.png';
-import WebHack from './images/web-hack.png';
 import { Typography } from '@material-ui/core';
+import Img from 'gatsby-image';
 
-// import Squiggle from './swiggly.js';
 import { ReactComponent as Squiggle } from './images/squiggle.svg';
 
 const prizeImageRadius = '50px';
 const allPrizes = [
 	{
-		img: MLHack,
+		img: 'ml-hack',
 		track: 'Best ML',
 		prize: 'Echo Dot'
 	},
 	{
-		img: DesignHack,
+		img: 'design-hack',
 		track: 'Best Design',
 		prize: 'Drawing Tablet'
 	},
 	{
-		img: MobileHack,
+		img: 'mobile-hack',
 		track: 'Best Mobile',
 		prize: 'Mini Phone Printer'
 	},
 	{
-		img: WebHack,
+		img: 'web-hack',
 		track: 'Best Web',
 		prize: 'Solar Phone Charger'
 	},
 	{
-		img: GameHack,
+		img: 'game-hack',
 		track: 'Best Game',
 		prize: 'Google Cardboard'
 	},
 	{
-		img: OverallHack,
+		img: 'overall-hack',
 		track: 'Best Overall',
 		prize: 'Mini Projector'
 	},
 	{
+		img: 'overall-hack',
 		track: '“You do You”',
 		prize: 'Seat Cushion'
 	}
 ];
 
-const TableCell = withStyles({
-	root: {
-		borderBottom: 'none'
-	}
-})(MuiTableCell);
-
 const useStyles = makeStyles({
 	prizeGrid: {
-		textAlign: 'center'
+		textAlign: 'center',
+		paddingBottom: '20px'
 	},
 	prizeCard: {
-		backgroundColor: '#e6e6e6',
+		backgroundColor: grey[200],
 		borderRadius: '10px',
 		height: '100%',
 		paddingTop: `${prizeImageRadius}`,
@@ -92,22 +74,18 @@ const useStyles = makeStyles({
 		fontSize: '1.5em'
 	},
 	imageBox: {
+		'--radius': '50px',
 		alignItems: 'center',
 		alignSelf: 'center',
 		backgroundColor: '#fafafa',
 		border: '6px solid',
-		borderColor: '#e6e6e6',
-		borderRadius: prizeImageRadius,
+		borderColor: grey[200],
+		borderRadius: 'var(--radius)',
 		display: 'flex',
 		justifyContent: 'center',
-		marginBottom: `calc(-1 * ${prizeImageRadius})`,
-		width: `calc(2 * ${prizeImageRadius})`,
-		height: `calc(2 * ${prizeImageRadius})`
-	},
-	prizeBox: {
-		display: 'flex',
-		justifyContent: 'center',
-		flexDirection: 'column'
+		marginBottom: `calc(-1 * var(--radius))`,
+		width: `calc(2 * var(--radius))`,
+		height: `calc(2 * var(--radius))`
 	},
 	item: {
 		fontWeight: 600,
@@ -117,69 +95,73 @@ const useStyles = makeStyles({
 
 export default function Prizes() {
 	const classes = useStyles();
-	const theme = useTheme();
-	const hideImg = useMediaQuery(theme.breakpoints.down('xs'));
+	const data = useStaticQuery(graphql`
+	{
+		prizeIcons: allFile(filter: {relativePath: {glob:"prize-icons/*"}}) {
+			nodes {
+				relativePath
+				childImageSharp {
+					fluid {
+						...GatsbyImageSharpFluid
+					}
+				}
+			}
+		}
+		prizeImages: allFile(filter: {relativePath: {glob:"prize-images/*"}}) {
+			nodes {
+				relativePath
+				childImageSharp {
+					fixed(height: 128) {
+						...GatsbyImageSharpFixed
+					}
+				}
+			}
+		}
+	}
+	`);
+	const iconImageMap = new Map();
+	for (const { relativePath, childImageSharp } of data.prizeIcons.nodes) {
+		const id = basename(relativePath).split('.')[0];
+		iconImageMap.set(id, childImageSharp.fluid);
+	}
 
+	const prizeImageMap = new Map();
+	for (const { relativePath, childImageSharp } of data.prizeImages.nodes) {
+		const id = basename(relativePath).split('.')[0];
+		prizeImageMap.set(id, childImageSharp.fixed);
+	}
 	return (
 		<>
 			<Container maxWidth='md'>
-				<Grid maxWidth='md' container spacing={3} className={classes.prizeGrid}>
+				<Grid container spacing={3} className={classes.prizeGrid}>
 					{allPrizes.map(prize =>
 						<Grid item xs={12} sm={4} key={prize.track}>
-							<Box className={classes.prizeBox}>
+							<Box display='flex' justifyContent='center' flexDirection='column'>
 								<Box className={classes.imageBox}>
-									<img src={prize.img} height='60px'/>
+									<Img fluid={iconImageMap.get(prize.img)} style={{ height: '70%', width: '72%' }} />
 								</Box>
 								<Box className={classes.prizeCard}>
 									<Typography variant='h6' component='h3'>
 										{prize.track}
 									</Typography>
-									<Box paddingLeft='20%' paddingRight='20%'>
+									<Box paddingX='20%'>
 										<Squiggle />
 									</Box>
 									<Box className={classes.prizeDescription}>
-										<Typography style={{ color: '#a1a1a1', fontSize: '14px' }}>
-											PRIZE
+										<Typography style={{ color: '#a1a1a1',
+											fontSize: '14px',
+											textTransform: 'uppercase' }}>
+											prize
 										</Typography>
 										<Typography>
 											{prize.prize}
 										</Typography>
 									</Box>
-									<img height='120px' src='https://upload.wikimedia.org/wikipedia/commons/6/6e/Golde33443.jpg'/>
+									<Img fixed={prizeImageMap.get(prize.img)} style={{ height: '128px' }} />
 								</Box>
 							</Box>
 						</Grid>)}
 				</Grid>
-			</Container>
-			<Container maxWidth='md'>
-				<TableContainer>
-					<Table aria-label="prize table">
-						<TableHead>
-							<TableRow>
-								{ hideImg ? null : <TableCell></TableCell> }
-								<TableCell align='center' className={classes.header}>Track</TableCell>
-								<TableCell align='center' className={classes.header}>Prize</TableCell>
-							</TableRow>
-						</TableHead>
-						<TableBody>
-							{allPrizes.map(row =>
-								<TableRow key={row.track}>
-									{ hideImg ?
-										null :
-										<TableCell align='center' width='20%'>
-											<img src={row.img} height='60px'/>
-										</TableCell>
-									}
-									<TableCell align='center' width='40%' className={classes.item}>
-										{row.track}
-									</TableCell>
-									<TableCell align='center' width='40%' className={classes.item}>
-										{row.prize}
-									</TableCell>
-								</TableRow>)}
-						</TableBody>
-					</Table>
-				</TableContainer>
 			</Container>
 		</>
 	);
