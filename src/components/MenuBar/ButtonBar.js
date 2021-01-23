6b5728bd1ec4167912210c430e6ab7d42bb3fab0
Timothy Gu <timothygu99@gmail.com>
2021-01-23 17:11:30 -0500

    Update ButtonBar.js

--- src/components/MenuBar/ButtonBar.js
+++ src/components/MenuBar/ButtonBar.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
 import PropTypes from 'prop-types';
-import classNames from 'classnames';
 import { makeStyles } from '@material-ui/core/styles';
 import { Link } from 'gatsby';
 
