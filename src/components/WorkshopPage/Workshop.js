b76d042f8df6a889c8a8b73cc45c48488510142d
Eric Yang <eric8yang@gmail.com>
2021-01-30 14:08:29 -0800

    updated labeler and some style changes

--- src/components/WorkshopPage/Workshop.js
+++ src/components/WorkshopPage/Workshop.js
@@ -12,14 +12,14 @@ const workshops = [
 		type: 'Web Development',
 		elements: [
 			{
-				title: 'Intro to HTML',
-				author: 'Eric',
+				title: 'Web Dev #1: HTML CSS JS',
+				author: 'Jakob Reinwald',
 				description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,',
 				url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstleyVEVO'
 			},
 			{
-				title: 'Intro to CSS',
-				author: 'Chandra',
+				title: 'Web Dev #2: React',
+				author: 'Nareh Agazaryan, Eric Yang',
 				description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,',
 				url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstleyVEVO'
 			},
@@ -35,8 +35,8 @@ const workshops = [
 		type: 'Mobile Development',
 		elements: [
 			{
-				title: 'Reactive Native',
-				author: 'Jakob',
+				title: 'Mobile Dev with React Native',
+				author: 'Einar Balan',
 				description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,',
 				url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstleyVEVO'
 			},
@@ -55,47 +55,54 @@ function Workshop() {
 	const isSmall = useMediaQuery(theme.breakpoints.down('sm'));
 
 	const workshopCards = workshops.map(item =>
-		<Grid key={item.type} style={{ paddingBottom: theme.spacing(isSmall ? 4 : 8) }}>
+		<React.Fragment key={item.type}>
 			<Typography
 				style={{
 					fontWeight: theme.typography.fontWeightMedium,
-					paddingBottom: theme.spacing(isSmall ? 4 : 8)
+					paddingBottom: theme.spacing(isSmall ? 2 : 4)
 				}}
-				variant='h3'
-				component='h2'
-			>
+				variant='h4'
+				component='h2'>
 				{item.type}
 			</Typography>
 
-			<Grid container spacing={8} justify='left'>
-				{item.elements.map(element => {
-					return (
-						<Grid key={element.title} item xs={12} sm={8} md={6}
-							style={{
-								paddingRight: theme.spacing(isSmall ? 4 : 8)
-							}}>
-							<ReactPlayer controls={true} width='100%' url={element.url} />
-							<Typography variant='h4' component='h3' style={{ paddingTop: '0.5em' }}>
-								{element.title}</Typography>
-							<Typography variant='h5' component='h3' style={{ paddingTop: '0.5em' }}>
-								{'Taught by: ' + element.author}</Typography>
-							<Typography variant='body1' style={{ paddingTop: '0.5em', paddingBottom: '0.5em' }}>
-								{element.description}
-							</Typography>
-							<Button variant='contained' disableElevation color="secondary" component='a'
-								href={element.url} target='_blank' rel='noreferrer noopener'
-								style={{ textTransform: 'none', padding: '4px 1.5em', maxWidth: 'fit-content' }}>
-								See Video
-							</Button>
-						</Grid>
-					);
-				})}
+			<Grid style={{ paddingBottom: theme.spacing(isSmall ? 4 : 8) }}>
+				<Grid container spacing={8} justify={isSmall ? 'center' : 'flex-start'}>
+					{item.elements.map(element => {
+						return (
+							<Grid key={element.title} item xs={12} sm={8} md={6}
+								style={{
+									paddingRight: theme.spacing(isSmall ? 4 : 8)
+								}}>
+								<ReactPlayer
+									style={{ borderRadius: '10px', overflow: 'hidden' }}
+									controls={true} width='100%' url={element.url} />
+								<hgroup>
+									<Typography variant='h5' component='h5' style={{ paddingTop: '0.5em' }}>
+										{element.title}
+									</Typography>
+									<Typography variant='h6' component='h6' style={{ paddingTop: '0.5em' }}>
+										{'Taught by: ' + element.author}
+									</Typography>
+								</hgroup>
+								<Typography variant='body1' style={{ paddingTop: '0.5em', paddingBottom: '0.5em' }}>
+									{element.description}
+								</Typography>
+								<Button variant='contained' disableElevation color="secondary" component='a'
+									href={element.url} target='_blank' rel='noreferrer noopener'
+									style={{ textTransform: 'none', padding: '4px 1.5em', maxWidth: 'fit-content' }}>
+									See Video
+								</Button>
+							</Grid>
+						);
+					})}
+				</Grid>
 			</Grid>
-		</Grid>);
+		</React.Fragment>);
 
 	return (
 		<Container maxWidth='lg' style={{ marginBottom: theme.spacing(8) }}>
-			<Typography variant='h2' component='h1'
+			<Typography variant='h3' component='h3'
 				style={{
 					fontWeight: theme.typography.fontWeightBold,
 					paddingTop: theme.spacing(isSmall ? 4 : 8),
