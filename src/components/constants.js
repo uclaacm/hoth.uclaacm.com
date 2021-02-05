20152906d42d68f6826c2e92ad26e85ec711c93a
Timothy Gu <timothygu99@gmail.com>
2021-02-04 20:51:31 -0500

    Fix hothStart constant

--- src/components/constants.js
+++ src/components/constants.js
@@ -1,3 +1,3 @@
-export const hothStart = new Date('2021-02-05T08:00:00-08:00');
+export const hothStart = new Date('2021-02-05T20:00:00-08:00');
 export const hothEnd = new Date('2021-02-07T21:00:00-08:00');
 export const applyDeadline = new Date('2021-02-01T23:59:59-08:00');
