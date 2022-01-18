3868471ec796881a66d57a048c688cf3682f7811
nathanzzhang <70558078+nathanzzhang@users.noreply.github.com>
2022-01-17 21:11:48 -0800

    added container

--- src/components/AnnouncementsPage/Announcements.js
+++ src/components/AnnouncementsPage/Announcements.js
@@ -2,19 +2,20 @@ import React from 'react';
 
 import Typography from '@material-ui/core/Typography';
 import useTheme from '@material-ui/core/styles/useTheme';
+import Container from '@material-ui/core/Container';
 
 export default function AnnouncementsPage() {
 	const theme = useTheme();
 
 	return (
-		<>
+		<Container>
 			<Typography variant='h4' component='h1'
 				style={{ fontWeight: theme.typography.fontWeightBold,
-					paddingTop: theme.spacing(37), // initial value 4
-					paddingBottom: theme.spacing(37), // initial value 4
+					paddingTop: theme.spacing(4),
+					paddingBottom: theme.spacing(4), 
 					textAlign: 'center' }}>
 				Check back here for announcements!
 			</Typography>
-		</>
+		</Container>
 	);
 }
