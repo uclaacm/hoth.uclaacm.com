cca5cf6f392ddd37e577370577d6952fd9fb17e3
khxia <17xiak124268@gmail.com>
2022-01-26 20:18:13 -0800

    fixes

--- .github/actions/update_announcements.py
+++ .github/actions/update_announcements.py
@@ -1,5 +1,9 @@
 import requests
+import os
+
+ISSUE_NUMBER = os.environ['ISSUE_NUMBER']
 
 if __name__ == "__main__":
-	print("updating announcements...")
+	print('updating announcements...')
+	print(f'From issue number: {ISSUE_NUMBER}')
 
