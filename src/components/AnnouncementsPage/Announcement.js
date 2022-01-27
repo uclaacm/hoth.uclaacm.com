5413ae5d2eea18cbc86b55ed5b4fc38339595114
Maggie Li <maggieelli@ucla.edu>
2022-01-27 00:53:18 -0800

    Fixed lint warnings

--- src/components/AnnouncementsPage/Announcement.js
+++ src/components/AnnouncementsPage/Announcement.js
@@ -2,34 +2,34 @@ import React from 'react';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import { useTheme } from '@material-ui/core/styles';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
-function Announcement(props) {
-    const theme = useTheme();
-    return (
-        <Container>
-            <Typography variant='h4' component='h2' style={{
-                color: theme.palette.primary.main,
-                fontWeight: theme.typography.fontWeightBold,
-				paddingTop: theme.spacing(4)}}>
+function Announcement() {
+	const theme = useTheme();
+	return (
+		<Container>
+			<Typography variant='h4' component='h2' style={{ color: theme.palette.primary.main,
+				fontWeight: theme.typography.fontWeightBold,
+				paddingTop: theme.spacing(4) }}>
                 HOTH 9 Date
 			</Typography>
-            <Typography variant='h7' component='h3' style={{
-                color: theme.palette.secondary.light,
-            }}>
+			<Typography variant='h7' component='h3' style={{
+				color: theme.palette.secondary.light
+			}}>
                 Posted on: January 27 at 11:59 PM PST
-            </Typography>
-            <Typography variant='h6' component='h4' style={{
-                paddingBottom: theme.spacing(4)
-            }}>
+			</Typography>
+			<Typography variant='h6' component='h4' style={{
+				paddingBottom: theme.spacing(4)
+			}}>
                 HOTH 9 is set to begin on Saturday, February 26. We look forward to seeing you then!
-            </Typography>
-        </Container>
-    );
+			</Typography>
+		</Container>
+	);
 }
 
-export default Announcement
+export default Announcement;
 
-Announcement.propTypes = {
-	
+/* Announcement.propTypes = {
+
 };
+*/
