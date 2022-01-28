641147bb4f4e100fca7f4fdaeff199973b1b2fca
khxia <17xiak124268@gmail.com>
2022-01-27 21:33:39 -0800

    added double check

--- .github/actions/update_announcements.py
+++
@@ -1,9 +0,0 @@
-import requests
-import os
-
-ISSUE_NUMBER = os.environ['ISSUE_NUMBER']
-
-if __name__ == "__main__":
-	print('updating announcements...')
-	print(f'From issue number: {ISSUE_NUMBER}')
-
