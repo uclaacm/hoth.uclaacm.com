3ab9ebbf10808618a61ba8d93c51da936a59423f
Maggie Li <69731426+maggieelli@users.noreply.github.com>
2022-02-03 19:16:55 -0800

    Created Announcement component, list, and JSON file (#192)
    * Created Announcement component
    
    * Fixed lint warnings
    
    * Created json file, mapped contents to Announcements page
    
    * Parsed timestamp, made style changes
    
    * Cleared JSON file

--- src/components/AnnouncementsPage/Announcements.js
+++ src/components/AnnouncementsPage/Announcements.js
@@ -1,21 +1,43 @@
 import React from 'react';
-
+import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
-import useTheme from '@material-ui/core/styles/useTheme';
 import Container from '@material-ui/core/Container';
+import Divider from '@material-ui/core/Divider';
+import Announcement from './Announcement';
+import data from '../../data/announcements.json';
+
+const useStyles = makeStyles(theme => ({
+	heading: {
+		fontWeight: theme.typography.fontWeightBold,
+		paddingTop: theme.spacing(4),
+		paddingBottom: theme.spacing(4),
+		textAlign: 'center'
+	}
+}));
 
 export default function AnnouncementsPage() {
-	const theme = useTheme();
+	const classes = useStyles();
 
 	return (
 		<Container maxWidth='md'>
-			<Typography variant='h4' component='h1'
-				style={{ fontWeight: theme.typography.fontWeightBold,
-					paddingTop: theme.spacing(4),
-					paddingBottom: theme.spacing(4),
-					textAlign: 'center' }}>
-				Check back here for announcements!
-			</Typography>
+			{data.length !== 0 ?
+				null :
+				<Typography variant='h4' component='h1' className={classes.heading}>
+							Check back here for announcements!
+				</Typography>}
+			{data.map(announcement =>
+				<>
+					<Announcement
+						key={announcement.id}
+						subject={announcement.subject}
+						timestamp={announcement.timestamp}
+						body={announcement.body}
+					/>
+					{announcement.id === data.length ?
+						null :
+						<Divider
+						/>}
+				</>)}
 		</Container>
 	);
 }
