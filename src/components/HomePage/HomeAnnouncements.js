858958cb42cb28632d2ac57808638dc8986e19be
nathanzzhang <70558078+nathanzzhang@users.noreply.github.com>
2022-02-10 02:29:24 -0800

    redesigned announcement on home

--- src/components/HomePage/HomeAnnouncements.js
+++ src/components/HomePage/HomeAnnouncements.js
@@ -2,46 +2,61 @@ import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
-import Box from '@material-ui/core/Box';
+//import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
-import Divider from '@material-ui/core/Divider';
 import Button from '@material-ui/core/Button';
 import data from '../../data/announcements.json';
+import PropTypes from 'prop-types';
 import { Link } from 'gatsby';
+import Collapse from '@material-ui/core/Collapse';
+//import { borders } from '@material-ui/system';
 
-import ExpandLessIcon from '@material-ui/icons/ExpandLess';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import ClearIcon from '@material-ui/icons/Clear';
+import IconButton from '@material-ui/core/IconButton';
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 
 const useStyles = makeStyles(theme => ({
 	subject: {
 		color: theme.palette.primary.main,
 		fontWeight: theme.typography.fontWeightBold,
-		paddingTop: theme.spacing(1),
-		textAlign: 'left',
+		textAlign: 'left'
 	},
-	date: {
-		color: theme.palette.secondary.light,
-		textAlign: 'center',
+	container: {
 		paddingTop: theme.spacing(1),
 		paddingBottom: theme.spacing(1)
 	},
+	date: {
+		color: theme.palette.secondary.light,
+		textAlign: 'right'
+	},
 	heading: {
-		paddingTop: theme.spacing(4),
+		paddingTop: theme.spacing(2),
+		paddingBottom: theme.spacing(2),
 		textAlign: 'left'
 	},
 	body: {
-		paddingTop: theme.spacing(1),
-		paddingBottom: theme.spacing(4),
+		paddingBottom: theme.spacing(2),
 		textAlign: 'left'
 	},
+	icon: {
+		padding: theme.spacing(2),
+		textAlign: 'right',
+		fontSize: '56px'
+	},
+	border: {
+		border: 10,
+		padding: theme.spacing(2),
+		borderColor: theme.palette.primary.main,
+		borderRadius: 16
+	},
 	link: {
-		textDecoration: 'none'
+		textDecoration: 'none',
+		textAlign: 'center'
 	},
 	box: {
 		justifyItems: 'center',
 		textAlign: 'center',
-		paddingBottom: theme.spacing(4)
+		paddingBottom: theme.spacing(1)
 	},
 	arrow: {
 		fontSize: '14px'
@@ -68,56 +83,56 @@ const useStyles = makeStyles(theme => ({
 	}
 }));
 
-function HomeAnnouncement({ subject, timestamp, body}) {
+function HomeAnnouncement({ subject, timestamp, body }) {
 	const classes = useStyles();
 	const d = new Date(timestamp);
 	const date = d.toLocaleDateString('en-US');
+	const time = d.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' });
 
 	return (
-		<Container>
-			<Grid container direction='column'>
-				<Grid container direction='row' justifyContent='space-between'>
-					<Grid item xs={9}>
+		<Grid container direction='column' justify='space-between'>
+			<Grid item>
+				<Grid container justify='space-between' alignItems='center'>
+					<Grid item>
 						<Typography variant='subtitle1' className={classes.subject}>
 							{subject}
 						</Typography>
 					</Grid>
-					<Grid item xs={3}>
+					<Grid item>
 						<Typography variant='subtitle2' className={classes.date}>
-							Posted on {date}
+							{date}, {time}
 						</Typography>
 					</Grid>
 				</Grid>
-				<Grid container direction='row' justifyContent='space-between' alignItems='center'>
-					<Grid item xs={9}>
-							<Typography variant='body2' className={classes.body}>
-								{body}
-							</Typography>
-					</Grid>
-					<Grid item
-						className={classes.box}
-						xs={3}
-						justifyContent='right'
-					>
-						<Link
-							to='/announcements'
-							className={classes.link}
-						>
-							<Button
-								className={classes.allAnnouncements}
-								target='_blank'
-							>
-								See All
-								<ArrowForwardIosIcon className={classes.arrow}></ArrowForwardIosIcon>
-							</Button>
-						</Link>
-					</Grid>
+				<Grid item>
+					<Typography variant='body2' className={classes.body}>
+						{body}
+					</Typography>
 				</Grid>
 			</Grid>
-			
-		</Container>
-	)
+			<Grid item>
+				<Link
+					to='/announcements'
+					className={classes.link}
+				>
+					<Button
+						className={classes.allAnnouncements}
+						target='_blank'
+					>
+						See All
+						<ArrowForwardIosIcon className={classes.arrow}></ArrowForwardIosIcon>
+					</Button>
+				</Link>
+			</Grid>
+		</Grid>
+	);
 }
+
+HomeAnnouncement.propTypes = {
+	subject: PropTypes.string,
+	timestamp: PropTypes.string,
+	body: PropTypes.string
+};
 
 export default function HomeAnnouncements() {
 	const classes = useStyles();
@@ -125,19 +140,34 @@ export default function HomeAnnouncements() {
 	const [dismissed, changeVisibility] = useState(0);
 
 	return (
-		<Container maxWidth='md'>
+		<Container maxWidth='lg' className={classes.container}>
 			{data.length === 0 ?
 				null :
 				<>
-					<Typography variant='h5' component='h1' className={classes.heading}>
-						Announcement:
-					</Typography>
-					<HomeAnnouncement
-						key={announcement.id}
-						subject={announcement.subject}
-						timestamp={announcement.timestamp}
-						body={announcement.body}
-					/>
+					<Collapse in={!dismissed} timeout={400}>
+						<Grid container direction='column' justify='space-between'>
+							<Grid container direction='row' justify='space-between' alignItems='center'>
+								<Grid item>
+									<Typography variant='h5' component='h1' className={classes.heading}>
+										Announcement
+									</Typography>
+								</Grid>
+								<Grid item>
+									<IconButton className={classes.icon} onClick={() => changeVisibility(a => !a)}>
+										<ClearIcon />
+									</IconButton>
+								</Grid>
+							</Grid>
+							<Grid item>
+								<HomeAnnouncement
+									key={announcement.id}
+									subject={announcement.subject}
+									timestamp={announcement.timestamp}
+									body={announcement.body}
+								/>
+							</Grid>
+						</Grid>
+					</Collapse>
 				</>}
 		</Container>
 	);
