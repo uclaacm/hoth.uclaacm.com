0c5be7f13318ce41d8359e75e0ea4f624be95075
nathanzzhang <70558078+nathanzzhang@users.noreply.github.com>
2022-02-10 20:34:11 -0800

    finalized homepage announcement

--- src/components/HomePage/HomeAnnouncements.js
+++ src/components/HomePage/HomeAnnouncements.js
@@ -2,14 +2,12 @@ import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
-// import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import data from '../../data/announcements.json';
 import PropTypes from 'prop-types';
-import { Link } from 'gatsby';
 import Collapse from '@material-ui/core/Collapse';
-// import { borders } from '@material-ui/system';
+import { navigate } from 'gatsby';
 
 import ClearIcon from '@material-ui/icons/Clear';
 import IconButton from '@material-ui/core/IconButton';
@@ -21,49 +19,34 @@ const useStyles = makeStyles(theme => ({
 		fontWeight: theme.typography.fontWeightBold,
 		textAlign: 'left'
 	},
-	container: {
-		paddingTop: theme.spacing(1),
-		paddingBottom: theme.spacing(1)
-	},
 	date: {
 		color: theme.palette.secondary.light,
+		paddingRight: theme.spacing(2),
 		textAlign: 'right'
 	},
-	heading: {
-		paddingTop: theme.spacing(2),
-		paddingBottom: theme.spacing(2),
-		textAlign: 'left'
-	},
 	body: {
+		paddingRight: theme.spacing(2),
 		paddingBottom: theme.spacing(2),
 		textAlign: 'left'
 	},
 	icon: {
-		padding: theme.spacing(2),
-		textAlign: 'right',
+		alignSelf: 'flex-end',
 		fontSize: '56px'
 	},
 	border: {
-		border: 10,
-		padding: theme.spacing(2),
-		borderColor: theme.palette.primary.main,
-		borderRadius: 16
+		border: '3px solid',
+		paddingLeft: theme.spacing(3),
+		paddingRight: theme.spacing(2),
+		paddingTop: theme.spacing(1),
+		paddingBottom: theme.spacing(2),
+		borderColor: theme.palette.primary.light,
+		borderRadius: 16,
+		marginTop: theme.spacing(2),
+		marginBottom: theme.spacing(2),
+		backgroundColor: '#fbf2ff'
 	},
-	link: {
-		textDecoration: 'none',
-		textAlign: 'center'
-	},
-	box: {
-		justifyItems: 'center',
-		textAlign: 'center',
-		paddingBottom: theme.spacing(1)
-	},
-	arrow: {
-		fontSize: '14px'
-	},
-	allAnnouncements: {
+	button: {
 		width: 'auto',
-		color: '#C960FF',
 		textAlign: 'center',
 		textDecoration: 'none',
 		fontSize: '14px',
@@ -72,13 +55,11 @@ const useStyles = makeStyles(theme => ({
 		margin: '5px 0px',
 		padding: '2px 1em',
 		maxWidth: 'fit-content',
-		border: '2px solid #C960FF',
+		border: '2px solid',
+		color: theme.palette.primary.main,
 		'&:hover': {
 			color: '#FFFFFF',
-			background: '#DB99FD'
-		},
-		[theme.breakpoints.down('sm')]: {
-			maxWidth: '100%'
+			background: theme.palette.primary.main
 		}
 	}
 }));
@@ -110,20 +91,14 @@ function HomeAnnouncement({ subject, timestamp, body }) {
 					</Typography>
 				</Grid>
 			</Grid>
-			<Grid item>
-				<Link
-					to='/announcements'
-					className={classes.link}
-				>
-					<Button
-						className={classes.allAnnouncements}
-						target='_blank'
-					>
-						See All
-						<ArrowForwardIosIcon className={classes.arrow}></ArrowForwardIosIcon>
-					</Button>
-				</Link>
-			</Grid>
+			<Button
+				variant='outlined'
+				className={classes.button}
+				endIcon={<ArrowForwardIosIcon />}
+				onClick={() => { navigate('/announcements') }}
+			>
+				See All
+			</Button>
 		</Grid>
 	);
 }
@@ -145,11 +120,11 @@ export default function HomeAnnouncements() {
 				null :
 				<>
 					<Collapse in={!dismissed} timeout={400}>
-						<Grid container direction='column' justify='space-between'>
+						<Grid container direction='column' justify='space-between' className={classes.border}>
 							<Grid container direction='row' justify='space-between' alignItems='center'>
 								<Grid item>
-									<Typography variant='h5' component='h1' className={classes.heading}>
-										Announcement
+									<Typography variant='h6' component='h2'>
+										Latest Announcement
 									</Typography>
 								</Grid>
 								<Grid item>
