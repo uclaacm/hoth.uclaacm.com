557207191840499e938c166e410f89556db7300f
datowq <steven.andrew.lew@gmail.com>
2023-01-31 17:27:42 -0800

    removed hover color change

--- src/components/GalleryPage/GalleryMenu.js
+++ src/components/GalleryPage/GalleryMenu.js
@@ -12,10 +12,7 @@ import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles(theme => ({
 	linkStyle: {
 		color: theme.palette.primary.dark,
-		textDecoration: 'none',
-		'&:hover': {
-			color: theme.palette.primary.main
-		}
+		textDecoration: 'none'
 	}
 }));
 
