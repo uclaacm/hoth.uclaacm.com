629f2f3d5ab2631bbbb6847c7bb4226efe316ca9
Katelyn <ydatabox@gmail.com>
2023-02-28 10:53:26 -0800

    fix typos, update blurbs

--- src/components/WorkshopPage/WorkshopPage.js
+++ src/components/WorkshopPage/WorkshopPage.js
@@ -62,7 +62,7 @@ const workshops = [
 				how to build your own version of Receiptify or Spotify Pie using spotify
 				APIs! In this workshop, we will learn all about using Web APIs to integrate
 				existing software systems (like Twitter, Venmo, Spotify, Google Maps) within
-				your OWN project! So, wrap up your Spotify playlist, and queue up this workshop
+				your OWN project! So wrap up your Spotify playlist, and queue up this workshop
 				on APIs!`,
 				youtube: 'https://youtu.be/U2MOLOC6GAI',
 				readme: 'https://github.com/uclaacm/hothX-workshops/tree/main/intro-to-apis',
@@ -71,7 +71,7 @@ const workshops = [
 			{
 				title: 'Intro to Servers',
 				author: 'Andy Lewis',
-				description: `The intro to servers workshop covers the basics of the client-server
+				description: `This intro to servers workshop covers the basics of the client-server
 				model and touches on relevant topics like hardware and software servers, HTTP,
 				and JSON. During the accompanying demo, you will learn how to create a simple
 				server and its API using JavaScript, Node.js, and Express. The server features
@@ -86,9 +86,9 @@ const workshops = [
 				title: 'Intro to React.js',
 				author: 'Brooke Jiang',
 				description: `In this workshop, we cover the basics of React with a simple demo
-				of creating a grocery list. We go over what React is and why we want to use it, make
+				of creating a grocery list. We will go over what React is and why we want to use it, make
 				our first component, and allow it to display different information using props and
-				array.map()! Furthermore, we learn the basics of State and useState, and how React magic
+				array.map()! Furthermore, we will learn the basics of State and useState, and how React magic
 				lets the user interact with the site. Shop no longer for ways to create your ideal website
 				and state tuned for this introduction to React!`,
 				youtube: 'https://youtu.be/1GNq5ez2Lxg',
@@ -119,7 +119,7 @@ const workshops = [
 			{
 				title: 'Hackathon 101',
 				author: 'Anan Wang',
-				description: `This workshop videos explains the basics of hackathons, which are events where
+				description: `This workshop video explains the basics of hackathons, which are events where
 				people work together to create technology projects. You'll learn what to expect and how to make
 				the most out of your experience. By the end of the video, you'll have a better understanding of
 				what hackathons are all about and how to get started with attending one. Happy hacking, handy
@@ -132,11 +132,12 @@ const workshops = [
 				title: 'Intro to Git',
 				author: 'Jenna Wang',
 				description: `Git is a distributed version control system; it brings a local copy of the
-				complete repository to every team member’s computer so they can commit, branch, and merge
-				locally. The benefits of using Git include the flexibility to work offline, reliable backup
-				copies, fast merging and flexible branching, rapid feedback, fewer merge conflicts, and available
-				support. We’ll be going over how to install and configure Git and how to use various Git commands
-				to switch branches, commit, push, and pull code, so let's 'git gud' at git!`,
+				complete repository to every team member’s computer, facilitating collaboration and easy
+				tracking of changes made to code. The benefits of using Git include the flexibility to
+				work offline, reliable backup copies, fast merging and flexible branching, rapid feedback,
+				fewer merge conflicts, and available support. We’ll be going over how to install and
+				configure Git and how to use various Git commands to switch branches, commit, push, and
+				pull code, so let's 'git gud' at git!`,
 				youtube: 'https://youtu.be/BefzoplcCzs',
 				readme: 'https://github.com/uclaacm/hothX-workshops/tree/main/intro-to-git',
 				slides: 'https://docs.google.com/presentation/d/1kzZsZGdJ4h_GscDhjbLPs4L3j0XPIpE1-F_w2_zc6N4/edit?usp=sharing'
@@ -156,7 +157,7 @@ const workshops = [
 			},
 			{
 				title: 'Intro to Game Development',
-				author: 'Austin Law',
+				author: 'Austin Law (ACM Game Studio)',
 				description: `It’s gamer time! In this workshop, we cover some general tips to game development.
 				Whether you want to make an action RPG or a visual novel, it's important to keep in mind where
 				to start, how to scale properly, and how to work with a team. Then, we briefly dive into Unity
