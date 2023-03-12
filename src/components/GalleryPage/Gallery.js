394e59d2490580edf452608dfc851af31b392b6a
Jakob Reinwald <75224269+jakobreinwald@users.noreply.github.com>
2023-03-11 21:14:19 -0800

    Jakob/maintenance (#331)
    * Fixed typos
    
    * Added rest of maintenance info to README
    
    * Fixed README typos
    
    * Paginated gallery carousel
    
    * Added cropped images

--- src/components/GalleryPage/Gallery.js
+++ src/components/GalleryPage/Gallery.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { graphql, useStaticQuery } from 'gatsby';
 import CarouselSlider from '../CarouselSlider/CarouselSlider';
 
-export default function Gallery() {
+Gallery.propTypes = {
+	hothName: PropTypes.string.isRequired
+};
+
+export default function Gallery({ hothName }) {
 	const data = useStaticQuery(graphql`
 		{
-			carouselImages: allFile(filter: {relativePath: {glob:"gallery-carousel/*"}}) {
+			carouselImages: allFile(filter: {relativePath: {glob:"gallery-carousel/*/*"}}) {
 				nodes {
 					id
 					relativePath
@@ -19,7 +24,12 @@ export default function Gallery() {
 		}
 	`);
 
+	const hothNameFolder = hothName.replace(' ', '-').toLowerCase();
+	const currentHothImages = data.carouselImages.nodes.filter(
+		node => node.relativePath.startsWith(`gallery-carousel/${hothNameFolder}/`)
+	);
+
 	return (
-		<CarouselSlider imageNodes={data.carouselImages.nodes} />
+		<CarouselSlider imageNodes={currentHothImages} />
 	);
 }
