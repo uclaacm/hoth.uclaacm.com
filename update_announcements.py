f965c1b67c471b45dc15aba8f2e2f89e41c07857
SPerrott22 <56176775+SPerrott22@users.noreply.github.com>
2024-02-04 15:41:09 -0800

    Added current dev team leads to allowlist

--- update_announcements.py
+++ update_announcements.py
@@ -10,7 +10,7 @@ import sys
 import os
 
 # User whitelist and issue number we are looking for
-user_whitelist =  ['khxia', 'jakobreinwald', 'maggieelli', 'katelynsyu', 'nareha']
+user_whitelist =  ['jakobreinwald', 'maggieelli', 'katelynsyu', 'jamesmwu', 'nathanzzhang', 'subramaniamsatyen']
 # HOTH repository issues accessed via Github API
 repo_issues = "https://api.github.com/repos/uclaacm/hoth.uclaacm.com/issues"
 
