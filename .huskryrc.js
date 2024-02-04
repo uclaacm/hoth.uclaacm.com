ddb83194fd178d933fe87ead59803947f52af3dd
Nathan Zhang <nathanz.zhang@gmail.com>
2024-02-04 15:49:59 -0800

    Edited again

--- .huskryrc.js
+++ .huskryrc.js
@@ -2,7 +2,7 @@ module.exports = {
 	hooks: {
 		// you can skip git hooks with --no-verify flag
 		// but make sure you know what you are doing
-		'pre-commit': 'echo "nonono"',
+		'pre-commit': 'echo \"nonono\"',
 		'pre-push': './.husky/pre-push'
 	}
 };
