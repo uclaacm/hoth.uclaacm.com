d8a6ba8347379244ac7aeaafbac0bb501d929bce
James Wu <jameswu21@g.ucla.edu>
2024-12-22 11:40:19 -0800

    Updated stylesheet name

--- src/components/Home/PhotoCarousel.jsx
+++ src/components/Home/PhotoCarousel.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import '../../styles/Gallery.css';
+import '../../styles/PhotoCarousel.css';
 
 import image1 from '../../images/carousel/image1.jpg';
 import image2 from '../../images/carousel/image2.jpg';
