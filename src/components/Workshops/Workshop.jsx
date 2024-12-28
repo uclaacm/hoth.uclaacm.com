0a2587c256a8dc1a16cf27a0e23ec3665c5212a1
Hannah Kendall <hbkendall@outlook.com>
2024-12-27 22:13:37 -0800

    Workshops Page

---
+++ src/components/Workshops/Workshop.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import '../../styles/Workshops.css';
+import ReactPlayer from 'react-player/youtube';
+import { Github, Youtube, File } from '@geist-ui/icons';
+
+
+export default function Workshop({ title, youtube, author, description, readme, slides}) {
+
+    
+    const README =  readme ?
+                    <button className='workshop-button'>
+                        <Github color='var(--hackAccent)' size={28} />
+                        <a className='workshop-link' href={readme} target='_blank' rel='noreferrer'>&nbsp; Readme</a>
+                    </button> : null;
+
+    const SLIDES =  slides ? 
+                    <button className='workshop-button'>
+                        <File color='var(--hackAccent)' size={28} />
+                        <a className='workshop-link' href={slides} target='_blank' rel='noreferrer'>&nbsp; Slides</a>
+                    </button> : null;
+    return (
+        <div className='workshop-container'>
+            <ReactPlayer 
+                className='workshop-video'
+                id={'id_' + title.replace(/ /g, '_')}
+                controls={true}
+                width='100%'
+                url={youtube} />
+            <hgroup className='workshop-header'>
+                <h3 className='workshop-title'>{title}</h3>
+                <h4 className='workshop-author'>Taught by: {author}</h4>
+            </hgroup>
+            <p className='workshop-description'>{description}</p>
+            <div className='workshop-links'>
+                <button className='workshop-button'>
+                    <Youtube color='var(--hackAccent)' size={28} />
+                    <a className='workshop-link' href={youtube} target='_blank' rel='noreferrer'>&nbsp; Video</a>
+                </button>
+                {README}
+                {SLIDES}
+            </div>
+        </div>
+    );
+}
