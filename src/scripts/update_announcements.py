58f10d9f1a4a9f5ffb35c68c8dca331f2e3698ae
Daniel Zhou <danielzh021@gmail.com>
2025-02-01 17:11:23 -0800

    change

--- src/scripts/update_announcements.py
+++ src/scripts/update_announcements.py
@@ -10,7 +10,7 @@ import sys
 import os
 
 # User whitelist and issue number we are looking for
-user_whitelist = ['jodymlin', 'khxia', 'jakobreinwald', 'christinatong01', 'milesswu', 'EinarBalan', 'danielhzhou']
+user_whitelist = ['jamesmwu', 'danielhzhou']
 # HOTH repository issues accessed via Github API
 repo_issues = "https://api.github.com/repos/uclaacm/hoth.uclaacm.com/issues"
 
