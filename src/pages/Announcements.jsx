d99715c938843cae1d438a71d492f89c39395865
Daniel Zhou <danielzh021@gmail.com>
2025-02-03 23:19:57 -0800

    annoucements

--- src/pages/Announcements.jsx
+++ src/pages/Announcements.jsx
@@ -1,19 +1,35 @@
 import React from 'react';
 import useTitle from '../components/General/useTitle';
+import announcements from '../data/announcements.json';
 import '../styles/Announcements.css';
 
 export default function Announcements() {
 	useTitle(' | Announcements');
 
+	const formatDate = (timestamp) => {
+		const date = new Date(timestamp);
+		return date.toLocaleDateString('en-US', {
+			year: 'numeric',
+			month: 'long',
+			day: 'numeric',
+			hour: '2-digit',
+			minute: '2-digit'
+		});
+	};
+
 	return (
 		<div id='announcements'>
-			<h1 className='announcements-title'>Announcements</h1>
-			<div className='announcements-container'>
-				<p>
-					HOTH XII will take place later this quarter, and our team is working
-					hard to organize a fantastic hackathon for you all!
-				</p>
-				<p>More news and announcements coming soon. Stay tuned!</p>
+			<h1>Announcements</h1>
+			<div>
+				{announcements.map((announcement) => (
+					<div key={announcement.id}>
+						<h2>{announcement.subject}</h2>
+						<p >{announcement.body}</p>
+						<p>
+							Posted on {formatDate(announcement.timestamp)}
+						</p>
+					</div>
+				))}
 			</div>
 		</div>
 	);
