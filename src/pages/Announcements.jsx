1828b24eeddcaa881641715b4298f7b0e70671b8
James Wu <jameswu21@g.ucla.edu>
2025-02-25 20:08:27 -0800

    Updated announcements.json location to properly serve in netlify deploy

--- src/pages/Announcements.jsx
+++ src/pages/Announcements.jsx
@@ -1,32 +1,32 @@
 import React from 'react';
 import useTitle from '../components/General/useTitle';
-import announcements from '../data/announcements.json';
+import announcements from '../../public/announcements.json';
 import '../styles/Announcements.css';
 
 export default function Announcements() {
 	useTitle(' | Announcements');
 
-	const formatDate = (timestamp) => {
+	const formatDate = timestamp => {
 		const date = new Date(timestamp);
 		return date.toLocaleDateString('en-US', {
 			weekday: 'long',
 			year: 'numeric',
-    		month: 'long',
-    		day: 'numeric',
+			month: 'long',
+			day: 'numeric',
 		});
 	};
 
 	return (
 		<div id='announcements'>
-			<h1 className = 'announcements-title'>Announcements</h1>
-			<div className = 'announcements-container'>
-				{announcements.map((announcement) => (
+			<h1 className='announcements-title'>Announcements</h1>
+			<div className='announcements-container'>
+				{announcements.map(announcement => (
 					<div key={announcement.id} className='announcements-label'>
-						<h2 className = 'announcements-subject'>{announcement.subject}</h2>
-						<p className = 'announcements-date'>
+						<h2 className='announcements-subject'>{announcement.subject}</h2>
+						<p className='announcements-date'>
 							{formatDate(announcement.timestamp)}
 						</p>
-						<p className = 'announcements-body'>{announcement.body}</p>
+						<p className='announcements-body'>{announcement.body}</p>
 					</div>
 				))}
 			</div>
