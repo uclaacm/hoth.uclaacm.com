cd11c832836b91b810f8aa1f721be6a3d472b4b1
James Wu <jameswu21@g.ucla.edu>
2025-02-25 20:13:01 -0800

    Updated announcements.jsx to fetch announcements at runtime to fix netlify build error

--- src/pages/Announcements.jsx
+++ src/pages/Announcements.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import useTitle from '../components/General/useTitle';
-import announcements from '../../public/announcements.json';
 import '../styles/Announcements.css';
 
 export default function Announcements() {
 	useTitle(' | Announcements');
+	const [announcements, setAnnouncements] = useState([]);
+
+	useEffect(() => {
+		fetch('/announcements.json')
+			.then(response => {
+				if (!response.ok) {
+					throw new Error('Network response was not ok');
+				}
+				return response.json();
+			})
+			.then(data => setAnnouncements(data))
+			.catch(error => console.error('Error fetching announcements:', error));
+	}, []);
 
 	const formatDate = timestamp => {
 		const date = new Date(timestamp);
@@ -20,15 +32,19 @@ export default function Announcements() {
 		<div id='announcements'>
 			<h1 className='announcements-title'>Announcements</h1>
 			<div className='announcements-container'>
-				{announcements.map(announcement => (
-					<div key={announcement.id} className='announcements-label'>
-						<h2 className='announcements-subject'>{announcement.subject}</h2>
-						<p className='announcements-date'>
-							{formatDate(announcement.timestamp)}
-						</p>
-						<p className='announcements-body'>{announcement.body}</p>
-					</div>
-				))}
+				{announcements.length > 0 ? (
+					announcements.map(announcement => (
+						<div key={announcement.id} className='announcements-label'>
+							<h2 className='announcements-subject'>{announcement.subject}</h2>
+							<p className='announcements-date'>
+								{formatDate(announcement.timestamp)}
+							</p>
+							<p className='announcements-body'>{announcement.body}</p>
+						</div>
+					))
+				) : (
+					<p>Loading announcements...</p>
+				)}
 			</div>
 		</div>
 	);
