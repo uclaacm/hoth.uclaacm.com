8ff6a81dbfac5dd9630ecbc7cb6439b862f6c3ee
James Wu <jameswu21@g.ucla.edu>
2025-02-27 18:16:15 -0800

    Updated prizes

--- src/data/PrizeData.js
+++ src/data/PrizeData.js
@@ -1,28 +1,28 @@
 const prizeData = [
-{
-    name: 'Best Overall',
-    amount: '$150 Amazon Gift Card'
-},
-{
-    name: 'Best Web',
-    amount: '$80 Amazon Gift Card'
-},
-{
-    name: 'Best Mobile',
-    amount: '$80 Amazon Gift Card'
-},
-{
-    name: 'Best Game',
-    amount: '$80 Amazon Gift Card'
-},
-{
-    name: 'Best in Intersectional Environmentalism',
-    amount: '2 Camping Hammocks and 2 Wireless Charging Pads'
-},
-{
-    name: 'Best Beginner',
-    amount: '4 Wireless 3-in-1 Charging Stations'
-}
-]
+	{
+		name: 'Best Overall',
+		amount: '$100 Amazon Gift Card',
+	},
+	{
+		name: 'Best Web',
+		amount: '$50 Amazon Gift Card',
+	},
+	{
+		name: 'Best Mobile',
+		amount: '$50 Amazon Gift Card',
+	},
+	{
+		name: 'Best Game',
+		amount: '$80 Amazon Gift Card',
+	},
+	{
+		name: 'Best in Tech for Kindness',
+		amount: '$100 Amazon Gift Card',
+	},
+	{
+		name: 'Best Beginner',
+		amount: '$50 Amazon Gift Card',
+	},
+];
 
 export default prizeData;
--- src/pages/Prizes.jsx
+++ src/pages/Prizes.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import useTitle from '../components/General/useTitle';
 import '../styles/Prizes.css';
-import prizeData from '../data/PrizeData'
+import prizeData from '../data/PrizeData';
 import squiggle from '../images/prizes/squiggle.svg';
 import overallIcon from '../images/prizes/overall.png';
 import webIcon from '../images/prizes/web.png';
@@ -14,14 +14,14 @@ import { LazyLoadImage } from 'react-lazy-load-image-component';
 export default function Prizes() {
 	useTitle(' | Prizes');
 
-	const getPrizeIcon = (name) => {
+	const getPrizeIcon = name => {
 		const iconMap = {
 			'Best Overall': overallIcon,
 			'Best Web': webIcon,
 			'Best Mobile': mobileIcon,
 			'Best Game': gameIcon,
-			'Best in Intersectional Environmentalism': ieIcon,
-			'Best Beginner': begIcon
+			'Best in Tech for Kindness': ieIcon,
+			'Best Beginner': begIcon,
 		};
 		return iconMap[name] || overallIcon;
 	};
@@ -30,22 +30,28 @@ export default function Prizes() {
 		<div id='prizes'>
 			<h1 className='prizes-title'>Prize Tracks</h1>
 			<div className='prizes-container'>
-				<p className='prizes-subtitle'>Below are the total prize amounts for each prize track! :)</p>
+				<p className='prizes-subtitle'>
+					Below are the prizes for each prize track! :)
+				</p>
 				<div className='prize-grid'>
-				{prizeData.map(prize => (
-					<div className='prize-card' key={prize.name}>
-					<div className='prize-card-icon-circle'>
-					<LazyLoadImage
-						src={getPrizeIcon(prize.name)}
-						alt={`${prize.name} Icon`}
-						className='prize-card-icon'
-					/>
-					</div>
-					<h2>{prize.name}</h2>
-					<LazyLoadImage src={squiggle} alt='Squiggle' className='card-squiggle' />
-					<p className='prize-label'>'PRIZE'</p>
-					<p className='prize-amount'>{prize.amount}</p>
-					</div>
+					{prizeData.map(prize => (
+						<div className='prize-card' key={prize.name}>
+							<div className='prize-card-icon-circle'>
+								<LazyLoadImage
+									src={getPrizeIcon(prize.name)}
+									alt={`${prize.name} Icon`}
+									className='prize-card-icon'
+								/>
+							</div>
+							<h2>{prize.name}</h2>
+							<LazyLoadImage
+								src={squiggle}
+								alt='Squiggle'
+								className='card-squiggle'
+							/>
+							<p className='prize-label'>PRIZE</p>
+							<p className='prize-amount'>{prize.amount}</p>
+						</div>
 					))}
 				</div>
 			</div>
