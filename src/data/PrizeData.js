f914fd206d588986a2ca9a1f18f351af2a5a7d6e
James Wu <jameswu21@g.ucla.edu>
2025-03-01 10:39:56 -0800

    - Updated schedule data - Updated prize data - Updated styling of prize grid

--- src/data/PrizeData.js
+++ src/data/PrizeData.js
@@ -5,23 +5,23 @@ const prizeData = [
 	},
 	{
 		name: 'Best Web',
-		amount: '$50 Amazon Gift Card',
+		amount: '$50 Amazon Gift Card and a 3-in-1 Foldable Wireless Charger',
 	},
 	{
 		name: 'Best Mobile',
-		amount: '$50 Amazon Gift Card',
+		amount: '$50 Amazon Gift Card and an Echo Dot',
 	},
-	{
-		name: 'Best Game',
-		amount: '$80 Amazon Gift Card',
-	},
+	// {
+	// 	name: 'Best Game',
+	// 	amount: '$80 Amazon Gift Card',
+	// },
 	{
 		name: 'Best in Tech for Kindness',
-		amount: '$100 Amazon Gift Card',
+		amount: '$50 Amazon Gift Card and a Camping Hammock',
 	},
 	{
 		name: 'Best Beginner',
-		amount: '$50 Amazon Gift Card',
+		amount: '$50 Amazon Gift Card and 4 Google Bags',
 	},
 ];
 
