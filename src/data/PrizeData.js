7ec71fcfb0d0775c76f33ccf1ec4b503c5e93401
Nishant Ray <43621741+Nishant-Ray@users.noreply.github.com>
2026-02-26 01:12:40 -0800

    Add oracle plushies to PrizeData.js (#595)

--- src/data/PrizeData.js
+++ src/data/PrizeData.js
@@ -1,27 +1,27 @@
 const prizeData = [
 	{
 		name: '1st Place Overall',
-		amount: '$300 Amazon Gift Card',
+		amount: '$300 Amazon Gift Card and Oracle Plushies',
 	},
 	{
 		name: '2nd Place Overall',
-		amount: '$200 Amazon Gift Card',
+		amount: '$200 Amazon Gift Card and Oracle Plushies',
 	},
 	{
 		name: '3rd Place Overall',
-		amount: '$100 Amazon Gift Card',
+		amount: '$100 Amazon Gift Card and Oracle Plushies',
 	},
 	{
 		name: 'Best AI',
-		amount: '$250 Amazon Gift Card',
+		amount: '$250 Amazon Gift Card and Oracle Plushies',
 	},
 	{
 		name: 'Best in Storytelling',
-		amount: '$150 Amazon Gift Card',
+		amount: '$150 Amazon Gift Card and Oracle Plushies',
 	},
 	{
 		name: 'Best Beginner',
-		amount: '$150 Amazon Gift Card',
+		amount: '$150 Amazon Gift Card and Oracle Plushies',
 	},
 ];
 
